0a9126cea1fc507adca4eaeebb5cbbabdfb50169
liujc <liujc@Ctrip.com>
2014-12-22 17:52:12 +0800

    fix a mistake

--- tikitoki/webapp/assets/js/core/utils/Date.js
+++ tikitoki/webapp/assets/js/core/utils/Date.js
@@ -1,87 +1,78 @@
 $import("assets.js.core.utils.AJKHelpers");
-
-Class.forName({
-	name : "class assets.js.core.utils.Date extends Object",
-	alias : "Date",
-
-	"public static parseOrig" : Date.parse,
-	"public static parse" : function(input) {
-		if (typeof input === "string") {
-			input = input.replace("ad", "").replace("AD", "");
-			var isBC = (input.toLowerCase().indexOf("bc") != -1);
-			input = input.replace("bc", "").replace("BC", "");
-			var largeYear = Date.AJKIsStringLargeYear(input);
-			if (largeYear) {
-				var retDate = new Date();
-				if (isBC) {
-					retDate.setFullYear(-largeYear);
-				} else {
-					retDate.setFullYear(largeYear);
-				}
-				return retDate;
-			} else if (isBC) {
-				var aDate = Date.parseOrig(input);
-				if (!aDate) {
-					return aDate;
-				}
-				aDate.setFullYear(-aDate.getFullYear());
-				return aDate;
+Date.parseOrig = Date.parse;
+Date.parse = function(input) {
+	if (typeof input === "string") {
+		input = input.replace("ad", "").replace("AD", "");
+		var isBC = (input.toLowerCase().indexOf("bc") != -1);
+		input = input.replace("bc", "").replace("BC", "");
+		var largeYear = Date.AJKIsStringLargeYear(input);
+		if (largeYear) {
+			var retDate = new Date();
+			if (isBC) {
+				retDate.setFullYear(-largeYear);
 			} else {
-				return Date.parseOrig(input);
+				retDate.setFullYear(largeYear);
 			}
+			return retDate;
+		} else if (isBC) {
+			var aDate = Date.parseOrig(input);
+			if (!aDate) {
+				return aDate;
+			}
+			aDate.setFullYear(-aDate.getFullYear());
+			return aDate;
 		} else {
 			return Date.parseOrig(input);
 		}
-	},
-
-	"public static STANDARDMAXFULLYEAR" : 99999,
-	"public static MAXFULLYEAR" : 999999999999,
-	"public static AJKLongDateActive" : true,
-	"public static AJKIsStringLargeYear" : function(input) {
-		input = input.replace(/ /g, "").replace(/,/g, "");
-		if (parseInt(input, 10) == input && input > Date.STANDARDMAXFULLYEAR && input < Date.MAXFULLYEAR) {
-			return input;
-		}
-		return false;
-	},
-
-	origSetFullYear : Date.prototype.setFullYear,
-	setFullYear : function(fullYear) {
+	} else {
+		return Date.parseOrig(input);
+	}
+};
+Date.STANDARDMAXFULLYEAR = 99999;
+Date.MAXFULLYEAR = 999999999999;
+Date.AJKLongDateActive = true;
+Date.AJKIsStringLargeYear = function(input) {
+	input = input.replace(/ /g, "").replace(/,/g, "");
+	if (parseInt(input, 10) == input && input > Date.STANDARDMAXFULLYEAR && input < Date.MAXFULLYEAR) {
+		return input;
+	}
+	return false;
+};
+Date.prototype.origSetFullYear = Date.prototype.setFullYear;
+Date.prototype.setFullYear = function(fullYear) {
+	if (fullYear > Date.STANDARDMAXFULLYEAR || fullYear < -Date.STANDARDMAXFULLYEAR) {
+		this.setFullYear(0);
+		this.ajkLongDate = true;
+		this.ajkLongDateYear = fullYear;
+	} else {
+		this.ajkLongDate = false;
+		return this.origSetFullYear(fullYear);
+	}
+};
+Date.prototype.origGetFullYear = Date.prototype.getFullYear;
+Date.prototype.getFullYear = function(fullYear) {
+	return (this.ajkLongDate) ? this.ajkLongDateYear : this.origGetFullYear();
+};
+Date.prototype.origGetTime = Date.prototype.getTime;
+Date.prototype.getTime = function(data) {
+	if (!Date.AJKLongDateActive) {
+		return this.origGetTime(data);
+	} else {
+		return (this.ajkLongDate) ? (this.ajkLongDateYear - 1970) * assets.js.core.utils.AJKHelpers.dateOneYearInMS : this.origGetTime();
+	}
+};
+Date.prototype.origSetTime = Date.prototype.setTime;
+Date.prototype.setTime = function(data) {
+	if (!Date.AJKLongDateActive) {
+		return this.origSetTime(data);
+	} else {
+		var fullYear = data / assets.js.core.utils.AJKHelpers.dateOneYearInMS + 1970;
 		if (fullYear > Date.STANDARDMAXFULLYEAR || fullYear < -Date.STANDARDMAXFULLYEAR) {
-			this.setFullYear(0);
 			this.ajkLongDate = true;
 			this.ajkLongDateYear = fullYear;
 		} else {
 			this.ajkLongDate = false;
-			return this.origSetFullYear(fullYear);
-		}
-	},
-	origGetFullYear : Date.prototype.getFullYear,
-	getFullYear : function(fullYear) {
-		return (this.ajkLongDate) ? this.ajkLongDateYear : this.origGetFullYear();
-	},
-	origGetTime : Date.prototype.getTime,
-	getTime : function(data) {
-		if (!Date.AJKLongDateActive) {
-			return this.origGetTime(data);
-		} else {
-			return (this.ajkLongDate) ? (this.ajkLongDateYear - 1970) * assets.js.core.utils.AJKHelpers.dateOneYearInMS : this.origGetTime();
-		}
-	},
-	origSetTime : Date.prototype.setTime,
-	setTime : function(data) {
-		if (!Date.AJKLongDateActive) {
 			return this.origSetTime(data);
-		} else {
-			var fullYear = data / assets.js.core.utils.AJKHelpers.dateOneYearInMS + 1970;
-			if (fullYear > Date.STANDARDMAXFULLYEAR || fullYear < -Date.STANDARDMAXFULLYEAR) {
-				this.ajkLongDate = true;
-				this.ajkLongDateYear = fullYear;
-			} else {
-				this.ajkLongDate = false;
-				return this.origSetTime(data);
-			}
 		}
 	}
-
-});
+};
